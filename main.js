10b461c6bf82d767501ea5ef2402e6bbd6d07431
Lisa M. Smith <lisamarie.smith@libertymutual.com>
2017-03-10 09:01:50 -0800

    created game logic

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+console.log("User flipped a " + cardOne);
+console.log("User flipped a " + cardTwo);
