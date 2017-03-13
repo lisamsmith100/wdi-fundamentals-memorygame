614b64b3521eb2278a1e2c95f038aefdbbe5d7d0
Lisa M. Smith <lisamarie.smith@libertymutual.com>
2017-03-12 18:37:44 -0700

    created functions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
-console.log("Up and running!");
+console.log("Let's play!");
 
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function (){
+	if ((cardsInPlay.length >= 2) && (cardsInPlay[0] === cardsInPlay[1])){
+		console.log("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+};
 
-console.log("User flipped a " + cardsInPlay[0]);
-console.log("User flipped a " + cardsInPlay[1]);
+var flipCard = function(cardId){
 
-if ((cardsInPlay.length >= 2) && (cardsInPlay[0] === cardsInPlay[1])){
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}
+	console.log("User flipped a " + cards[cardId] + ".");
+
+	cardsInPlay.push(cards[cardId]);
+// 	checkForMatch();
+
+
+// delete var cardOne = cards[0];
+// delete cardsInPlay.push(cardOne);
+// delete console.log("User flipped a " + cardsInPlay[0]);
+
+
+// delete var cardTwo = cards[2];
+// delete cardsInPlay.push(cardTwo);
+// delete console.log("User flipped a " + cardsInPlay[1]);
+
+};
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
+//flipCard(0);
+//flipCard(2);
+
+//	if ((cardsInPlay.length >= 2) && (cardsInPlay[0] === cardsInPlay[1])){
+//		alert("You found a match!");
+//	} else {
+//		alert("Sorry, try again.");
+//	}
