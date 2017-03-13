0846edbe43a8dce395594e577d66d2c80507eb01
Lisa M. Smith <lisamarie.smith@libertymutual.com>
2017-03-13 12:06:30 -0700

    added more game logic - objects yay!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 console.log("Let's play!");
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{
+		rank: "Queen",
+		suit: "Hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+
+	{
+		rank: "Queen",
+		suit: "Diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+
+	{
+		rank: "King",
+		suit: "Hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "King",
+		suit: "Diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function (){
@@ -13,9 +36,12 @@ var checkForMatch = function (){
 
 var flipCard = function(cardId){
 
-	console.log("User flipped a " + cards[cardId] + ".");
+	console.log("User flipped a " + cards[cardId].rank + ".");
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit)
 // 	checkForMatch();
 
 
