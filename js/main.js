5e52d07fc8f03ee1581924097e230953fe50315c
Lisa M. Smith <lisamarie.smith@libertymutual.com>
2017-03-14 14:10:54 -0700

    completed Chap 11 Assignment

--- js/main.js
+++ js/main.js
@@ -27,40 +27,37 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function (){
-	if ((cardsInPlay.length >= 2) && (cardsInPlay[0] === cardsInPlay[1])){
-		console.log("You found a match!");
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 };
 
-var flipCard = function(cardId){
-
-	console.log("User flipped a " + cards[cardId].rank + ".");
-
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute("src", cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-
+	console.log("User flipped a " + cards[cardId].rank + ".");
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit)
-// 	checkForMatch();
-
-
-// delete var cardOne = cards[0];
-// delete cardsInPlay.push(cardOne);
-// delete console.log("User flipped a " + cardsInPlay[0]);
-
-
-// delete var cardTwo = cards[2];
-// delete cardsInPlay.push(cardTwo);
-// delete console.log("User flipped a " + cardsInPlay[1]);
-
+	if(cardsInPlay.length == 2){
+		checkForMatch();
+	}
 };
-flipCard(0);
-flipCard(2);
-checkForMatch();
 
-//flipCard(0);
-//flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img'); 
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
 
 //	if ((cardsInPlay.length >= 2) && (cardsInPlay[0] === cardsInPlay[1])){
 //		alert("You found a match!");
